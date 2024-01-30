e640402ad17e2714d8ab66bc2266125199839051
李浩瀚 <lihaohan@ft.tech>
2024-01-30 16:46:34 +0800

    feat: tt

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,9 @@ use if_chain::if_chain;
 use once_cell::sync::Lazy;
 use std::thread;
 use std::time::Duration;
-use sysinfo::{Components, Disks, Networks, System};
+
 
 fn main() {
-    let mut sys = System::new_all();
-
-    // First we update all information of our `System` struct.
-    sys.refresh_all();
-
-    println!("used swap   : {} bytes", sys.global_cpu_info().name());
-
+    let linux = sys_info::linux_os_release().unwrap();
+    println!("{:?}",linux);
 }
