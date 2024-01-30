746dad77046041ddc27e1f579390224d3c1ea14e
李浩瀚 <lihaohan@ft.tech>
2024-01-30 16:18:20 +0800

    feat: tt

--- src/main.rs
+++ src/main.rs
@@ -8,27 +8,9 @@ use raw_cpuid::CpuId;
 
 fn main() {
     let cpuid = CpuId::new();
-
-    if let Some(vf) = cpuid.get_vendor_info() {
-        assert!(vf.as_str() == "GenuineIntel" || vf.as_str() == "AuthenticAMD");
-    }
-
-    let has_sse = cpuid
-        .get_feature_info()
-        .map_or(false, |finfo| finfo.has_sse());
-    if has_sse {
-        println!("CPU supports SSE!");
-    }
-
-    if let Some(cparams) = cpuid.get_cache_parameters() {
-        for cache in cparams {
-            let size = cache.associativity()
-                * cache.physical_line_partitions()
-                * cache.coherency_line_size()
-                * cache.sets();
-            println!("L{}-Cache size is {}", cache.level(), size);
-        }
+    if let Some(sif) = cpuid.get_processor_serial() {
+        println!("Serial number: {:?}", sif.serial_all());
     } else {
-        println!("No cache parameter information available")
+        println!("No serial number information available");
     }
 }
