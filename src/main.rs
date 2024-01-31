6202961de0b1ccb65039baffe1314f36e129ca4f
李浩瀚 <lihaohan@ft.tech>
2024-01-31 09:34:34 +0800

    feat: add init_linux_disk_info

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,16 @@ use if_chain::if_chain;
 use once_cell::sync::Lazy;
 use std::thread;
 use std::time::Duration;
-
+pub fn init_ip_info()->anyhow::Result<()>{
+    let my_local_ip = local_ip_address::local_ip()?;
+    println!("{:?}",my_local_ip);
+    Ok(())
+}
 
 fn main() {
-    let linux = sys_info::linux_os_release().unwrap();
-    println!("{:?}",linux);
+    if let Err(e) = init_ip_info() {
+        println!("Error: {}", e);
+    }
+
+    
 }
