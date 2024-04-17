94cf38a72d6cbd2dcafe515b423894154aa1be6e
fawkesLi <lihaohan@ft.tech>
2024-04-17 15:39:56 +0800

    tt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::{borrow::Borrow, io::Read};
+
 pub mod st;
 
 fn fa(asdasd: i32, b: B) -> i32 {
@@ -13,7 +15,8 @@ impl B {
     fn bb(&self) -> i32 {
         let r = || {
             let a = 1;
-            a
+            let one = st::sta::A::aa();
+            a + one
         };
         r()
     }
@@ -36,4 +39,12 @@ fn main() {
     println!("{}", c);
     let d = aa();
     println!("{}", d);
+    let sled = sled::open("ph").unwrap();
+    let vv = sled.scan_prefix("fn");
+    for v in vv {
+        let (k,v) = v.unwrap();
+        let key = String::from_utf8(k.to_vec()).unwrap();
+        let value = String::from_utf8(v.to_vec()).unwrap();
+        println!("key: {},\n value: {}\n\n", key, value);
+    }
 }
